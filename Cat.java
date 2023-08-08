d747636a799131aebe8984ea25ad9804ca440b93
flori.butuc <f.butuc5@gmail.com>
2023-08-08 13:01:48 +0300

    catBorn add

--- Cat.java
+++ Cat.java
@@ -3,12 +3,11 @@ package EX_OOP.ex_3_catBorn;
 import java.util.Scanner;
 
 public class Cat {
-    static String name = "";
     static int counterCatBorn = 0;
 
     public static int getCounterCatBorn() {
         Scanner sc = new Scanner(System.in);
-        name = sc.nextLine();
+        String name = sc.nextLine();
         if (!(name.isEmpty())) {
             ++counterCatBorn;
         }
--- Main.java
+++ Main.java
@@ -1,7 +1,4 @@
 package EX_OOP.ex_3_catBorn;
-
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         System.out.print(Cat.getCounterCatBorn());
